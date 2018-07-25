e1f2771e29cc7060710048dda14e0374ab71ab34
Matt Moran <matt.moran@connect-group.com>
2018-07-25 14:44:00 +0100

    Some details plus changing city

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 //Init weather object
 const weather = new Weather('London', 'UK');
 const ui = new UI();
+// Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
+
+//Location change event
+document.getElementById('w-change-btn' ).addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+    weather.changeLocation(city, state);
+    getWeather();
+
+    //close modal
+    $('#locModal').modal('hide');
+});
+
 function getWeather() {
     weather.getWeather()
     .then(results => ui.paint(results))
--- ui.js
+++ ui.js
@@ -5,9 +5,9 @@ class UI {
         this.string = document.getElementById('w-string');
         this.details = document.getElementById('w-details');
         this.icon = document.getElementById('w-icon');
-        this.humidity = document.getElementById('w-hunidity');
-        this.feelslike = document.getElementById('w-feels-like');
+        this.humidity = document.getElementById('w-humidity');
         this.dewpoint = document.getElementById('w-dewpoint');
+        this.wind = document.getElementById('w-wind');
 
     }
 
@@ -15,6 +15,10 @@ class UI {
         console.log(weather);
         this.location.textContent = weather.name;
         this.desc.textContent = weather.weather[0].main;
-        this.details.textContent = weather.weather[0].description;
+        this.string.textContent = Math.round((weather.main.temp - 273) * 100 + Number.EPSILON) / 100;
+        //this.details.textContent = weather.weather[0].description;
+        this.icon.setAttribute('src', `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`);
+        this.humidity.textContent = `Humidity: ${weather.main.humidity}`;
+        this.wind.textContent = `Wind Heading: ${weather.wind.deg} degrees, speed: ${weather.wind.speed} kph`;
     }
 }
